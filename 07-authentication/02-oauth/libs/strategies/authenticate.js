d4902aad14e25eb248a59664b202151eb35b013a
Begaiym Adylbek kyzy <mrmldsky27@gmail.com>
2022-10-15 19:42:12 +0200

    add solution for 7-module 2-task

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,25 @@
+const User = require('../../models/User');
+
 module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+  if (!email) {
+    return done(null, false, 'Не указан email');
+  }
+
+  User.findOne({ email }, async (err, user) => {
+    if (err) {
+      done(err);
+    }
+
+    if (user) {
+      return done(null, user);
+    }
+
+    try {
+      const u = new User({ email, displayName });
+      await u.save();
+      done(null, u);
+    } catch (err) {
+      done(err);
+    }
+  });
 };
