0af24ba7ba951112a5486612be63c6f0d8bd8436
Begaiym Adylbek kyzy <mrmldsky27@gmail.com>
2022-12-10 15:39:38 +0100

    add solution for 8-module 2-task

--- 08-email/02-order/models/Order.js
+++ 08-email/02-order/models/Order.js
@@ -1,7 +1,31 @@
 const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
+const {ObjectId} = mongoose.Schema.Types;
+
 const orderSchema = new mongoose.Schema({
+  user: {
+    type: ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  product: {
+    type: ObjectId,
+    required: true,
+    ref: 'Product',
+  },
+  phone: {
+    type: String,
+    required: true,
+    validate: [{
+      validator: (val) => /\+?\d{6,14}/.test(val),
+      message: 'Неверный формат номера телефона.',
+    }],
+  },
+  address: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = connection.model('Order', orderSchema);
